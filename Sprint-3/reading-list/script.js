ff6e271e5b7eb62c3e947ce0638bff38dc7f7861
TzeMingHo <tzemingho@gmail.com>
2025-10-27 12:57:20 +0000

    can't pass the background color test

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,29 @@ const books = [
   },
 ];
 
+function renderReadingList() {
+  let readingList = document.getElementById("reading-list");
+
+  books.forEach((book) => {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    const img = document.createElement("img");
+
+    img.src = book.bookCoverImage;
+
+    p.textContent = `${book.title} by ${book.author}`;
+
+    const backgroundColor = book.alreadyRead
+      ? "rgb(0, 128, 0)"
+      : "rgb(255, 0, 0)";
+    li.style.backgroundColor = backgroundColor;
+    li.setAttribute("style", `background-color: ${backgroundColor};`);
+
+    li.appendChild(p);
+    li.appendChild(img);
+    readingList.appendChild(li);
+    console.log(li);
+  });
+}
+
+renderReadingList();
