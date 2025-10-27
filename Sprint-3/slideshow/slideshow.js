a226feceae18f5f0452faa2ec4a2349fcf3ab29a
TzeMingHo <tzemingho@gmail.com>
2025-10-27 16:25:41 +0000

    added an input field for auto play seconds

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -9,40 +9,45 @@ const images = [
 function createSlideShow() {
   let currentImageIndex = 0;
   let image = document.getElementById("carousel-img");
-  let auto = false;
   let intervalId;
 
   return {
     forward() {
-      this.stop();
       currentImageIndex === images.length - 1
         ? (currentImageIndex = 0)
         : currentImageIndex++;
       image.src = images[currentImageIndex];
     },
     backwards() {
-      this.stop();
       currentImageIndex === 0
         ? (currentImageIndex = images.length - 1)
         : currentImageIndex--;
       image.src = images[currentImageIndex];
     },
-    autoForward() {
-      intervalId = setInterval(() => {
-        this.forward();
-      }, 3000);
+    autoForward(second) {
+      this.stop();
+      if (second >= 1) {
+        intervalId = setInterval(() => {
+          this.forward();
+        }, second * 1000);
+      }
     },
     stop() {
       clearInterval(intervalId);
+    },
+    autoBackward(second) {
+      this.stop();
+      if (second >= 1) {
+        intervalId = setInterval(() => {
+          this.backwards();
+        }, second * 1000);
+      }
     },
   };
 }
 
 const slideShow = createSlideShow();
 
-document.getElementById("stop-btn").addEventListener("click", () => {
-  slideShow.stop();
-});
 document.getElementById("forward-btn").addEventListener("click", () => {
   slideShow.forward();
 });
@@ -50,5 +55,13 @@ document.getElementById("backward-btn").addEventListener("click", () => {
   slideShow.backwards();
 });
 document.getElementById("auto-forward-btn").addEventListener("click", () => {
-  slideShow.autoForward();
+  let second = document.getElementById("auto-seconds").value;
+  slideShow.autoForward(second);
+});
+document.getElementById("stop-btn").addEventListener("click", () => {
+  slideShow.stop();
+});
+document.getElementById("auto-backward-btn").addEventListener("click", () => {
+  let second = document.getElementById("auto-seconds").value;
+  slideShow.autoBackward(second);
 });
